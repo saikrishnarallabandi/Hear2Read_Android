4527513e3b88a425d20c3177f1c29cd754fc6168
saikrishna.lticmu <saikrishna.r@research.iiit.ac.in>
2017-02-09 07:46:50 -0500

    temp

---
+++ gen_old/org/hear2read/kannada/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.hear2read.kannada;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ gen_old_2/org/hear2read/Marathi/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.hear2read.Marathi;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
